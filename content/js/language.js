ac65c3e467daaf0683cbc3d488427c5d70df65f4
Latonal <reflexion85@gmail.com>
2022-04-11 15:27:47 +0200

    Pushing to publish first second version

--- content/js/language.js
+++ content/js/language.js
@@ -11,9 +11,11 @@ let language = {
     "id": {
         "fr": {
             "text_id_01":"texte",
+            "text_id_07":"Un plus petit pourcentage ne veut pas nécessaire dire que je ne connais pas des parties du langage, cela signifie que j'aurais plus de difficulté à produire le résultat attendu.",
         },
         "en": {
             "text_id_01":"texte",
+            "text_id_07":"A lower percentage does not mean I do not know part of the language, it means I might have more struggles to do some stuff.",
         }
     }
 }
